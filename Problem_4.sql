c83a972b01800db5c1c903e7b2126925d481abc6
carlos_costa <carloscosta.pen@gmail.com>
2026-03-13 20:27:17 -0300

    Finding total cars number

---
+++ Problem_4.sql
@@ -0,0 +1,16 @@
+use VehicleMakesDB;
+
+
+
+
+select Makes.Make, count(*)as NumberOfVehicles,
+(select  count(*) from VehicleDetails ) as TotalVehicles
+from VehicleDetails 
+inner join Makes on VehicleDetails.MakeID = Makes.MakeID
+where VehicleDetails.Year between 1950 and 2000
+group by Makes.Make
+order by NumberOfVehicles desc
+
+
+
+	 
