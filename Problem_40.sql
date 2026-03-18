c75c2f35abf50ecbe7627719632af8be0290333e
carlos_costa <carloscosta.pen@gmail.com>
2026-03-17 23:41:15 -0300

    Get Number of doors Made by ford

---
+++ Problem_40.sql
@@ -0,0 +1,9 @@
+-- Get Make and Total Number Of Doors Manufactured Per ford
+
+use VehicleMakesDB;
+
+
+select Make , sum(NumDoors) as TotalNumOfDoors from VehicleDetails
+inner join Makes on Makes.MakeID = VehicleDetails.MakeID
+group by Make
+having Make = 'Ford'
